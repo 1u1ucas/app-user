f530696d7b0313f9ad8bc4796afc73374dbe3b58
1u1ucas <lucasleveque17@gmail.com>
2025-01-28 09:22:59 +0100

    -modification du create du score pour ajotuer le username-

--- src/pages/api/score/create.ts
+++ src/pages/api/score/create.ts
@@ -17,11 +17,29 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         return res.status(400).json({ error: 'Invalid data format' });
       }
 
+      //récupère le username avec le playerId
+      const { data: playerData, error: playerError } = await supabase
+      .from('user')
+      .select('*')
+      .or(`playerId.eq.${playerId}`)
+      .single();
+
+      if (playerError) {
+        console.error("Erreur lors de la récupération de l'utilisateur:", playerError); // Log l'erreur détaillée
+        return res.status(500).json({ error: playerError.message || 'Erreur inconnue lors de la récupération de l\'utilisateur' });
+      }
+
+      if (!playerData) {
+        return res.status(404).json({ error: 'Utilisateur introuvable' });
+
+      }
+
+      const username = playerData.username;
 
       // Insère le score dans la base de données
       const { data, error } = await supabase
         .from('score')
-        .insert([{ playerId, gameId, score }]) // Assure-toi d'envoyer un tableau d'objets, même pour une seule insertion
+        .insert([{ playerId, gameId, score, username }]) // Assure-toi d'envoyer un tableau d'objets, même pour une seule insertion
         .select();
 
       if (error) {
