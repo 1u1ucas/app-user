eee76764c2aef684016c9e95bdb8472e71720443
1u1ucas <lucasleveque17@gmail.com>
2025-01-28 19:17:16 +0100

    --

--- src/components/leaderBoardButton.tsx
+++ src/components/leaderBoardButton.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 type BoardButtonProps = {
   activity: number;
@@ -6,17 +7,30 @@ type BoardButtonProps = {
 };
 
 export default function LeaderBoardButton({ activity, setActivity }: BoardButtonProps) {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 640); // Tailwind's sm breakpoint
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <div className="relative flex background rounded-full h-fit w-fit gap-12">
+    <div className="relative flex flex-col self-center sm:flex-row background sm:rounded-full rounded-3xl h-fit sm:w-full w-52 gap-2 sm:gap-12">
       {/* Effet de slide */}
       <motion.div
-        className="absolute bg-[#00C8C8] rounded-full h-full w-1/5"
-        animate={{ x: `${activity * 100}%` }}
+        className="absolute bg-[#00C8C8] rounded-full self-center"
+        style={{ width: isMobile ? `calc(100% / 1.5)` : `calc(100% / 5)`, height: isMobile ? `calc(100% / 5)` : '100%' }}
+        animate={isMobile ? { y: `${activity * 100}%` } : { x: `${activity * 100}%` }}
         transition={{ type: "spring", stiffness: 300, damping: 20 }}
       />
       <button
         onClick={() => setActivity(0)}
-        className={`relative z-10 flex-1 font-bold py-2 rounded-full whitespace-nowrap m-2 w-32 ${
+        className={`relative z-10 flex-1 font-bold py-2 rounded-full whitespace-nowrap m-1 sm:m-2 w-full sm:w-32 ${
           activity === 0 ? "text-black" : "text-white"
         }`}
       >
@@ -24,7 +38,7 @@ export default function LeaderBoardButton({ activity, setActivity }: BoardButton
       </button>
       <button
         onClick={() => setActivity(1)}
-        className={`relative z-10 flex-1 self-center font-bold py-2 rounded-full m-2 w-32 ${
+        className={`relative z-10 flex-1 font-bold py-2 rounded-full whitespace-nowrap m-1 sm:m-2 w-full sm:w-32 ${
           activity === 1 ? "text-black" : "text-white"
         }`}
       >
@@ -32,7 +46,7 @@ export default function LeaderBoardButton({ activity, setActivity }: BoardButton
       </button>
       <button
         onClick={() => setActivity(2)}
-        className={`relative z-10 flex-1 self-center font-bold py-2 rounded-full m-2 w-32 ${
+        className={`relative z-10 flex-1 font-bold py-2 rounded-full whitespace-nowrap m-1 sm:m-2 w-full sm:w-32 ${
           activity === 2 ? "text-black" : "text-white"
         }`}
       >
@@ -40,7 +54,7 @@ export default function LeaderBoardButton({ activity, setActivity }: BoardButton
       </button>
       <button
         onClick={() => setActivity(3)}
-        className={`relative z-10 flex-1 self-center font-bold py-2 rounded-full m-2 w-32 ${
+        className={`relative z-10 flex-1 font-bold py-2 rounded-full whitespace-nowrap m-1 sm:m-2 w-full sm:w-32 ${
           activity === 3 ? "text-black" : "text-white"
         }`}
       >
@@ -48,7 +62,7 @@ export default function LeaderBoardButton({ activity, setActivity }: BoardButton
       </button>
       <button
         onClick={() => setActivity(4)}
-        className={`relative z-10 flex-1 self-center font-bold py-2 rounded-full m-2 w-32 ${
+        className={`relative z-10 flex-1 font-bold py-2 rounded-full whitespace-nowrap m-1 sm:m-2 w-full sm:w-32 ${
           activity === 4 ? "text-black" : "text-white"
         }`}
       >
