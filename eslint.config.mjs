5a77ed4b88763bc8f73890c94464e98d42e6f93e
1u1ucas <lucasleveque17@gmail.com>
2025-01-28 09:36:51 +0100

    --

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable specific ESLint rules here
+      "no-unused-vars": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
